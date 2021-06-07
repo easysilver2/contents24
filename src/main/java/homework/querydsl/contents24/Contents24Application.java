6ff888600011f68ede510d56831ad6a80babed3d
이지은 <jelee@snack24h.com>
2021-06-07 10:38:14 +0900

    Initial commit

---
+++ src/main/java/homework/querydsl/contents24/Contents24Application.java
@@ -0,0 +1,13 @@
+package homework.querydsl.contents24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Contents24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Contents24Application.class, args);
+	}
+
+}
---
+++ src/test/java/homework/querydsl/contents24/Contents24ApplicationTests.java
@@ -0,0 +1,13 @@
+package homework.querydsl.contents24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Contents24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
