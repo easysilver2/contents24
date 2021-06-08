399cd68a9a642c64472a4aa4e63576b74db1cf33
이지은 <jelee@snack24h.com>
2021-06-08 11:43:40 +0900

    페이징 조회 및 정렬 테스트 코드 추가

--- src/test/java/homework/querydsl/contents24/entity/PlatformTest.java
+++ src/test/java/homework/querydsl/contents24/entity/PlatformTest.java
@@ -1,5 +1,6 @@
 package homework.querydsl.contents24.entity;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -50,17 +51,59 @@ class PlatformTest {
     }
 
     @Test
-    void 플랫폼_조건_검색() {
+    void 플랫폼_and조건_검색() {
         Platform findPlatform = queryFactory
-                .selectFrom(QPlatform.platform)
-                .where(QPlatform.platform.name.eq("유튜브")
-                        .and(QPlatform.platform.link.contains("youtube")))
+                .selectFrom(platform)
+                .where(platform.name.eq("유튜브")
+                        .and(platform.link.contains("youtube")))
                 .fetchOne();
 
         assertThat(findPlatform.getLink()).isEqualTo("youtube.com");
         assertThat(findPlatform.getName()).isEqualTo("유튜브");
     }
 
+    @Test
+    void 페이징용_결과조회() {
+        //given
+        QueryResults<Platform> results = queryFactory
+                .selectFrom(platform)
+                .fetchResults();
+        //when
+        List<Platform> platforms = results.getResults();
+        long totalCount = results.getTotal();
+
+        //then
+        assertThat(platforms.size()).isEqualTo(totalCount);
+    }
+
+    @Test
+    void 플랫폼_정렬() {
+        //given
+        em.persist(Platform.builder()
+                .name("프로그래머스")
+                .link("programmers.com")
+                .build());
+
+        em.persist(Platform.builder()
+                .name("노마드코더")
+                .link("nomardcoders.com")
+                .build());
+
+        //when
+        List<Platform> platforms = queryFactory
+                .selectFrom(platform)
+                // 플랫폼명 오름차순(가나다순)으로 정렬
+                .orderBy(platform.name.asc())
+                .fetch();
+
+        for (Platform pf : platforms) {
+            System.out.println(">>>\t\tpf = " + pf.getName());
+        }
+
+        //then
+        assertThat(platforms.size()).isEqualTo(4);
+        assertThat(platforms.get(0).getName()).isEqualTo("노마드코더");
+    }
 
     @Test
     public void testJPQL() {
