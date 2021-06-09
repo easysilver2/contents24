3f78a56fef292ca4c1fff55ea4d5be5174ee3474
이지은 <jelee@snack24h.com>
2021-06-09 10:30:06 +0900

    테스트 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO platform(platform_name, platform_link) VALUES('인프런', 'inflearn.com');
+INSERT INTO platform(platform_name, platform_link) VALUES('유튜브', 'youtube.com');
+INSERT INTO platform(platform_name, platform_link) VALUES('프로그래머스', 'programmers.com');
