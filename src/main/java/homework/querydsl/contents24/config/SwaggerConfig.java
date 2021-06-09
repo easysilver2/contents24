c024163496e56660c825b52cab47756085336b45
이지은 <jelee@snack24h.com>
2021-06-09 10:30:22 +0900

    Swagger 메인 화면 설정

--- src/main/java/homework/querydsl/contents24/config/SwaggerConfig.java
+++ src/main/java/homework/querydsl/contents24/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package homework.querydsl.contents24.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,11 +14,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Contents 24")
+                .description("Querydsl 실습 과제")
+                .build();
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(this.apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors
+                        .basePackage("homework.querydsl.contents24.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
