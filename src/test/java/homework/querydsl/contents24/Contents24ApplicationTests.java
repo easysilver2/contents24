cb5b5b70c7283a11a691c4f451007f9f2832fd1b
이지은 <jelee@snack24h.com>
2021-06-09 10:34:05 +0900

    사용하지 않는 테스트 코드 삭제

--- src/test/java/homework/querydsl/contents24/Contents24ApplicationTests.java
+++ src/test/java/homework/querydsl/contents24/Contents24ApplicationTests.java
@@ -1,38 +1,13 @@
 package homework.querydsl.contents24;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import homework.querydsl.contents24.entity.Hello;
-import homework.querydsl.contents24.entity.QHello;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@Transactional
 @SpringBootTest
 class Contents24ApplicationTests {
 
-	@Autowired
-	EntityManager em;
-
 	@Test
 	void contextLoads() {
-		Hello hello = new Hello();
-		em.persist(hello);
-
-		JPAQueryFactory query = new JPAQueryFactory(em);
-		QHello qHello = QHello.hello;
-
-		Hello result = query
-				.selectFrom(qHello)
-				.fetchOne();
-
-		assertThat(result).isEqualTo(hello);
-		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
