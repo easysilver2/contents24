44e372c10c7d0090e71dd0f244442e8fd724ab3d
이지은 <jelee@snack24h.com>
2021-06-11 11:04:58 +0900

    유효성 검증 로직 수정 (빈 스페이스도 통과되지 않도록)

--- src/main/java/homework/querydsl/contents24/dto/PlatformRequestDto.java
+++ src/main/java/homework/querydsl/contents24/dto/PlatformRequestDto.java
@@ -3,24 +3,19 @@ package homework.querydsl.contents24.dto;
 import homework.querydsl.contents24.entity.Platform;
 import io.swagger.annotations.ApiParam;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@NoArgsConstructor
+/**
+ * 플랫폼 요청 관련 DTO
+ */
 @Getter @Setter
 public class PlatformRequestDto {
 
     @ApiParam(value = "플랫폼 이름", required = true, example = "노마드코더")
     private String name;
+
     @ApiParam(value = "플랫폼 링크", required = true, example = "https://nomadcoders.co/")
     private String link;
-
-    public PlatformRequestDto(String name, String link) {
-        this.name = name;
-        this.link = link;
-    }
 
     public Platform toEntity() {
         return Platform.builder()
@@ -31,11 +26,10 @@ public class PlatformRequestDto {
 
     /* 신규 등록 입력 값 유효성 검증 */
     public void checkValidation() {
-        if (name == null || name.isEmpty())
+        if (name == null || name.isBlank())
             throw new IllegalArgumentException("플랫폼 이름 값이 입력되지 않았습니다.");
 
-        if(link == null || name.isEmpty())
+        if(link == null || link.isBlank())
             throw new IllegalArgumentException("플랫폼 링크 값이 입력되지 않았습니다.");
     }
-
 }
